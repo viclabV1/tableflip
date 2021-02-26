d4e55ab01dd6e38d64112e0bc15d37c3cf14b8f4
viclabV1 <vplabrie@gmail.com>
2021-02-25 23:16:03 -0600

    Started adding role config option, and another stupid meme

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const DotEnv = require('dotenv').config()
 const { userInfo } = require('os');
 const client = new Discord.Client();
+
 //const userMan = new Discord.UserManager();
 //const channelMan = new Discord.ChannelManager();
 //const guildMan = new Discord.GuildManager();
@@ -20,15 +21,24 @@ client.on('message', msg => {
     console.log('ඞ');
     msg.reply('ඞ');
   }
+
+//config roles
+/*
+if (msg.content.includes('!roleconfig')&& !(msg.content.includes(helpMessage))){
+  console.log('role config');
+  if(msg.member.hasPermission('ADMINISTRATOR')){
+    role = msg.content.replace('!roleconfig', '');
+  }
+  else msg.reply('You are not an admin. Bitch. Stupid fucking baby.');
+}
+*/
 //this was too dumb
-/*  
+
   if (msg.content === 'AMOGUS') {
     console.log('ඞඞඞඞඞඞ');
-    for(i=0; i<10; i++){
-      msg.reply('ඞ');
-    }
+    msg.reply('\n⠀⠀‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎ ‎‎‎‎‎⠀⣠⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⢰⡿⠋⠁⠀⠀⠈⠉⠙⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⢀⣿⠇⠀⢀⣴⣶⡾⠿⠿⠿⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⣀⣀⣸⡿⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⣾⡟⠛⣿⡇⠀⠀⢸⣿⣿⣷⣤⣤⣤⣤⣶⣶⣿⠇⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀ ⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣷ ⢸⣿⠀⢸⣿⡇⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⣿⡇⣀⣠⣴⣾⣮⣝⠿⢿⣿⣻⡟ ⢸⣿⠀⠘⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠉⠀ ⠸⣿⠀⠀⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀ ⠀⠻⣷⣶⣿⣇⠀⠀⠀⢠⣼⣿⣿⣿⣿⣿⣿⣿⣛⣛⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⢸⣿⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀ ⠀⠀ ⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⣿')
   }
-*/
+
 //tableflip command
   if (msg.content === '!tableflip') {
     console.log('Table flip started');
@@ -37,7 +47,7 @@ client.on('message', msg => {
       msg.channel.delete();
       console.log('Table flipped');
     }
-    else msg.reply('You are not an admin.');
+    else msg.reply('You do not have the correct role.');
   }
 //tableclear command
   if (msg.content.includes('!tableclear')&& !(msg.content.includes(helpMessage))){
@@ -45,16 +55,16 @@ client.on('message', msg => {
     if(msg.member.hasPermission('ADMINISTRATOR')){
       num = Math.round(msg.content.replace('!tableclear ', ''));
       if((num>=1)&&(num<=100)){
-          msg.channel.bulkDelete(num)
-		.then(console.log(`Cleared ` + num + ' messages'))
-  		.catch(console.error);
+        msg.channel.bulkDelete(num)
+        .then(console.log(`Cleared ` + num + ' messages'))
+        .catch(console.error);
         }
       else {
         msg.reply('Invalid Input! Use the "!help" command to review the syntax.');
         msg.reply('Also make sure the number you input is between 1 and 100!');
       }
     }
-    else msg.reply('You are not an admin. Bitch. Stupid fucking baby.');
+    else msg.reply('Wrong role. Bitch. Stupid fucking baby bitch boy.');
   }
 //nuclear command reundant as an admin can just delete a channel with two clicks anyhow
 /*
@@ -71,7 +81,6 @@ client.on('message', msg => {
 helpMessage = 'This bot allows admins to clear messages from a channel. This can be done in 2 ways. The first is by typing using the "!tableclear" command. '
 +'This command will delete a specified number of messages from a channel. It can be called in the following way: "!tableclear [number between 1 and 100]". '
 +'The second way to clear messages is by using the "!tableflip" command. Simply enter "!tableflip" to delete all messages from a channel. Amogus is also a command you can use. If you want. I hate myself for including it but whatever.';
-
 
 
 client.login(process.env.BOT_LOGIN);
